218f7058f59a01a18314ddb4d92dce9a4826a7ac
Shiwangi Kumari <sshiwangi770@gmail.com>
2022-11-11 23:48:53 +0530

    November 9

---
+++ November Leetcode daily challenge/901. Online Stock Span.cpp
@@ -0,0 +1,61 @@
+/* Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+
+The span of the stock's price in one day is the maximum number of consecutive days (starting from that day and going backward) for which the stock price was less than or equal to the price of that day.
+
+For example, if the prices of the stock in the last four days is [7,2,1,2] and the price of the stock today is 2, then the span of today is 4 because starting from today, the price of the stock was less than or equal 2 for 4 consecutive days.
+Also, if the prices of the stock in the last four days is [7,34,1,2] and the price of the stock today is 8, then the span of today is 3 because starting from today, the price of the stock was less than or equal 8 for 3 consecutive days.
+Implement the StockSpanner class:
+
+StockSpanner() Initializes the object of the class.
+int next(int price) Returns the span of the stock's price given that today's price is price.
+ 
+Example 1:
+
+Input
+["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
+[[], [100], [80], [60], [70], [60], [75], [85]]
+Output
+[null, 1, 1, 1, 2, 1, 4, 6]
+
+Explanation
+StockSpanner stockSpanner = new StockSpanner();
+stockSpanner.next(100); // return 1
+stockSpanner.next(80);  // return 1
+stockSpanner.next(60);  // return 1
+stockSpanner.next(70);  // return 2
+stockSpanner.next(60);  // return 1
+stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
+stockSpanner.next(85);  // return 6
+ 
+
+Constraints:
+
+1 <= price <= 105
+At most 104 calls will be made to next. */
+
+//code
+
+class StockSpanner {
+public:
+stack<pair<int,int>>st; // it will store the stock price along with a count that stores how many prices are lesser than this price
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+       int count = 1; // we declare the count 1 cause we will also consider the price 
+       while(!st.empty() && st.top().first <= price){ //check if the top of the stack is lesser than the current price, then keep popping out the previous prices until the top of stack is lesser and also add their count to current price's count cause their count stores how many prices were lesser than those prices.
+           int second = st.top().second;
+           st.pop();
+           count += second;
+       }
+       st.push({price, count}); // then push the price wiht their count in the stack and return the count.
+       return count;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
