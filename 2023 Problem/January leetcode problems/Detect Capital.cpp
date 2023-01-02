0c0746fba718e534f29f60b6418180340803ff81
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-01-02 12:33:35 +0530

    Update Detect Capital.cpp

--- 2023 Problem/January leetcode problems/Detect Capital.cpp
+++ 2023 Problem/January leetcode problems/Detect Capital.cpp
@@ -54,3 +54,33 @@ public:
         return flag;
     }
 };
+
+// Second Method
+/* Intuition: use a counter and isupper function to check number of capital.
+
+Approach:
+
+Initialize count=0
+if count equals 0 means all are small chars
+if count equals size of word then all are capital
+if count equals 1 then check is it the first char which is capital
+if none of the conditions are true then return false */
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        if(word.size()==1)
+            return true;
+        
+        for(int i=0; i<word.size(); i++)
+            if(isupper(word[i]))
+                count++;
+        
+        if(count==1 && isupper(word[0]))
+            return true;
+        if(count==0 || count==word.size())
+            return true;
+        else
+            return false;
+    }
+};
