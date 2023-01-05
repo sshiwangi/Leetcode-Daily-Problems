40e3263fcc6dc2bb988547edf56841746dae3407
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-01-05 22:12:09 +0530

    Update Delete Columns to Make Sorted.cpp

--- 2023 Problem/January leetcode problems/Delete Columns to Make Sorted.cpp
+++ 2023 Problem/January leetcode problems/Delete Columns to Make Sorted.cpp
@@ -1,6 +1,6 @@
 /* You are given an array of n strings strs, all of the same length.
 
-The strings can be arranged such that there is one on each line, making a grid. For example, strs = ["abc", "bce", "cae"] can be arranged as:
+The strings can be arranged such that there is one on each line, making a grid. For example, strs = ["abc", "bce", "cae"] can be arranged as
 
 abc
 bce
