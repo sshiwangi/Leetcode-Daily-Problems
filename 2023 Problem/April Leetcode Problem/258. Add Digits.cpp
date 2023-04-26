ad7fec2484ab7a9f5023702681c24535f972a18a
Shiwangi Kumari <sshiwangi770@gmail.com>
2023-04-26 23:52:03 +0530

    Create 258. Add Digits.cpp

---
+++ 2023 Problem/April Leetcode Problem/258. Add Digits.cpp
@@ -0,0 +1,34 @@
+/* Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= num <= 231 - 1 */
+//code
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        while(num>0){
+            int rem = num%10;
+            sum += rem;
+            num = num/10;
+        }
+        if(sum<10)return sum;
+        else return addDigits(sum);
+    }
+};
